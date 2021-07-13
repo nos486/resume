721fc3f2f3068c2160ef9c3b462f6757a68a284c
sina <nos486@gmail.com>
2021-07-13 14:58:55 +0430

    First

---
+++ src/components/Avatar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <img src="@/assets/avatar.jpeg" class="avatar shadow" :style="{width:size,height:size}">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Avatar",
+    props: {
+        size: {
+            type: String,
+            default : "200px"
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.avatar {
+    vertical-align: middle;
+    border-radius: 50%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/js/all'
+
 Vue.config.productionTip = false
 
 new Vue({
