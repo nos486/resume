3cc3dcdedc9027923ecd4bce117731037224e890
Sina <nos486@gmail.com>
2022-01-17 23:49:47 +0330

    Update Contact.vue

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,7 +3,7 @@
         <item title="Contact">
 
             <IconText icon="fa fa-map-marked-alt" text="Tehran, Iran"></IconText>
-            <IconText icon="fa fa-phone" text="+98 912 8895140" href="tel: +989128895140"></IconText>
+            <IconText icon="fa fa-phone" text="+98 912 889XXXX" href="tel: +98912889XXXX"></IconText>
             <IconText icon="fab fa-google" text="Nos486@gmail.com" href="mailto: nos486@gmail.com"></IconText>
             <IconText icon="fab fa-github" text="Github.com/nos486" href="https://github.com/nos486"></IconText>
             <IconText icon="fab fa-linkedin" text="Linkedin.com/in/nos486" href="https://www.linkedin.com/in/nos486/"></IconText>
